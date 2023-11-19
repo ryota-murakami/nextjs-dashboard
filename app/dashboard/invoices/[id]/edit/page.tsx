c56e8458554ebd1c9e0240b5b4b1b5712fec2601
ryota-murakami <dojce1048@gmail.com>
2023-11-19 13:45:15 +0900

    Completed Chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,13 +1,19 @@
+import { notFound } from 'next/navigation'
+
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data'
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs'
 import Form from '@/app/ui/invoices/edit-form'
-
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
   ])
+
+  if (!invoice) {
+    notFound()
+  }
+
   return (
     <main>
       <Breadcrumbs
