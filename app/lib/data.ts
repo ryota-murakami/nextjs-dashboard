e51d85ffadba904bcc5c3590e004f6305aeb05fa
ryota-murakami <dojce1048@gmail.com>
2023-11-30 05:31:56 +0900

    Remove unnecessary code and console logs from data and search components

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,14 +13,8 @@ import type {
 import { formatCurrency } from './utils'
 
 export async function fetchRevenue() {
-  // Add noStore() here prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore()
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    console.log('Fetching revenue data...')
     await new Promise((resolve) => setTimeout(resolve, 3000))
 
     const data = await sql<Revenue>`SELECT * FROM revenue`
